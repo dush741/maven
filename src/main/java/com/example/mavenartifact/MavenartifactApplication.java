a851499e5a6ef0734567d90ed146a053869a6b98
dush741 <dushyanth736@gmail.com>
2021-06-14 12:15:41 +0530

    mavenproject

---
+++ src/main/java/com/example/mavenartifact/MavenartifactApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavenartifact/MavenartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
